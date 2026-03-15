34c3dd9017edf83819fad603d03113f260f2276f
kazurayam <kazuaki.urayama@gmail.com>
2026-03-15 22:03:59 +0900

    save updates

--- Include/scripts/groovy/com/kazurayam/ks/DriverFactoryModifierTest.groovy
+++ Include/scripts/groovy/com/kazurayam/ks/DriverFactoryModifierTest.groovy
@@ -1,9 +1,11 @@
 package com.kazurayam.ks
 
+import com.kms.katalon.core.webui.driver.DriverFactory
 import static org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 @RunWith(JUnit4.class)
 public class DriverFactoryModifierTest {
@@ -12,5 +14,8 @@ public class DriverFactoryModifierTest {
 	public void testSmoke() {
 		boolean b = DriverFactoryModifier.runWith("Chrome")
 		assertTrue(b)
+		WebUI.openBrowser('')
+		WebUI.delay(1)
+		WebUI.closeBrowser()
 	}
 }
--- Keywords/com/kazurayam/ks/DriverFactoryModifier.groovy
+++ Keywords/com/kazurayam/ks/DriverFactoryModifier.groovy
@@ -1,13 +1,44 @@
 package com.kazurayam.ks
 
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.WebDriver
-
-import com.kms.katalon.core.webui.driver.DriverFactory
+import com.kms.katalon.core.logging.LogLevel;
+import com.kms.katalon.core.webui.driver.DriverFactory;
+import com.kms.katalon.core.driver.IDriverType;
+import com.kms.katalon.core.exception.StepFailedException;
 
 public class DriverFactoryModifier {
 
 	public static boolean runWith(String browser) {
-		println "run with " + browser
+		println "[DriverFactoryModifier#runWith] " + browser
+		DriverFactory.metaClass.static.openWebDriver = { ->
+			println "[DriverFactory#openWebDriver] "
+			/**
+			 * Open a new WebDriver based on the RunConfiguration
+			 */
+			try {
+				WebDriver webDriver;
+				if (DriverFactory.isUsingExistingDriver()) {
+					webDriver = DriverFactory.startExistingBrowser();
+				} else {
+					String remoteWebDriverUrl = DriverFactory.getRemoteWebDriverServerUrl();
+					if (StringUtils.isNotEmpty(remoteWebDriverUrl())) {
+						webDriver = DriverFactory.startRemoteBrowser();
+					} else {
+						IDriverType executedBrowser = DriverFactory.getExecutedBrowser();
+						println "[DriverFactory++] executedBrowser: " + executedBrowser.getName();
+						webDriver = DriverFactory.startNewBrowser(executedBrowser);
+					}
+					DriverFactory.saveWebDriverSessionData(webDriver);
+					DriverFactory.changeWebDriver(webDriver);
+				}
+				return webDriver
+			} catch (Error e) {
+				DriverFactory.logger.logMessage(LogLevel.WARNING, e.getMessage(), e);
+				throw new StepFailedException(e);
+			}
+		}
+		println "[DriverFactoryModifier] modified"
 		return true
 	}
 }
